3b63d1ebd1d40123ace8e7f8b53814036a6b29be
Ata <atakanozalan@gmail.com>
2023-04-11 02:44:32 +0300

    advice label, color changing, calculations are done

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Ezagor on 11.04.2023.
+//
+
+import Foundation
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -5,23 +5,42 @@
 //  Created by Ezagor on 7.04.2023.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmi: Float?
+    var bmi: BMI?
     
     mutating func calculateBMI(height: Float, weight: Float)  {
-        bmi = weight / pow(height, 2)
+       let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Zargana", color: UIColor.blue)
+            
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Maşallah!", color: UIColor.green)
+            
+        }else{
+            bmi = BMI(value: bmiValue, advice: "Panda panda panda", color: UIColor.red)
+        }
+        
+        
         //        bmiValue = String(format: "%.1f", bmi)
     }
     
     func getBMIValue() -> String {
         
-        let bmiTo1DecimalPlace = String(format: "%.1f", bmi ?? 0.0)
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
             return bmiTo1DecimalPlace
-            
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "Tavsiye yok"
+    }
+    
+    func getColor() -> UIColor {
         
+        return bmi?.color ?? UIColor.white
         
     }
     
